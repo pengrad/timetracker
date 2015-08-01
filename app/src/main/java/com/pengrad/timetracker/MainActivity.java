b6f555a613140a6d3c6f4207bb9b042a37591b61
stas <pengrad@bk.ru>
2015-08-01 21:09:43 +0300

    Styling main activity

--- app/src/main/java/com/pengrad/timetracker/MainActivity.java
+++ app/src/main/java/com/pengrad/timetracker/MainActivity.java
@@ -2,6 +2,7 @@ package com.pengrad.timetracker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
 
         ListView listView = (ListView) findViewById(R.id.listview);
 
